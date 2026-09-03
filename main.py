1a31e17bfe1221aaaa97598211d870ff626001be
rudrapatel-1368 <rudra1368patel@gmail.com>
2026-09-03 10:58:03 +0530

    added the gemini api key

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
+import google.generativeai as genai
 
 class BusinessInput(BaseModel):
     raw_text: str
@@ -19,4 +20,11 @@ def ingest_input(data: BusinessInput):
     if len(data.raw_text.strip()) < 20:
         return {"Error" : "Please provide more info"}
     return {"revieved" : data.raw_text}
+
+#Calling an actual API of gemini
+load_dotenv()
+Gkey = os.getenv("GEMINI_API_KEY")
+
+genai.configure(api_key=Gkey)
+model = genai.GenerativeModel("gemini-2.5-flash")
 
